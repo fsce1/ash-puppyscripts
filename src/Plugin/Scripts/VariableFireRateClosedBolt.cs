2aa245eeb4598d3ea4191ea2d00393200da217d8
fsce <jamieserlin@gmail.com>
2022-06-25 03:22:48 +0100

    Added ChangeBaseRotOffsetOnAttach, DisableOnAttached, VariableFireRateClosedBolt, VariableFireRateOpenBolt.

---
+++ src/Plugin/Scripts/VariableFireRateClosedBolt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FistVR;
+namespace ashpuppyscripts
+{
+    public class VariableFireRateClosedBolt : MonoBehaviour
+    {
+        public ClosedBolt ClosedBolt;
+        public float minSpringStiffness = 100;
+        public float maxSpringStiffness = 1000;
+        private bool HasChanged = false;
+        public void FixedUpdate()
+        {
+            if (!HasChanged && ClosedBolt.transform.position == ClosedBolt.Point_Bolt_Forward.position)
+            {
+                HasChanged = true;
+                ClosedBolt.SpringStiffness = Random.Range(minSpringStiffness, maxSpringStiffness);
+
+            }
+            else if (HasChanged && ClosedBolt.transform.position != ClosedBolt.Point_Bolt_Forward.position)
+            {
+                HasChanged = false;
+            }
+        }
+
+    }
+
+}
